744a5c612613cd6fe0f924f5c5e5a0c5a05248ff
olivergirard <girard1@purdue.edu>
2024-03-06 21:36:28 -0500

    basic checkbox system

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,89 @@
 #include "ImGui/imgui_impl_glfw.h"
 #include "ImGui/imgui_impl_opengl3.h"
 
-int main() {
+using namespace std;
 
+const int screenWidth = 800;
+const int screenHeight = 800;
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "CS434 Final Project", NULL, NULL);
+
+	if (window == NULL)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	/* Place all functions required to build the scene here. */
+
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+	bool drawScene = true;
+	bool filled = false;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("CS434 Final Project");
+
+		if (ImGui::Button("Tongwei")) {
+
+		}
+		if (ImGui::Button("Sei")) {
+
+		}
+		if (ImGui::Button("Oliver")) {
+
+		}
+
+		ImGui::End();
+
+		
+		/* Do matrix creation here. */
+
+		if (drawScene) {
+
+			/* Do all OpenGL drawing here. */
+
+		}
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	
+	/* Ensure you delete VAOs, VBOs, and shader programs! */
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
