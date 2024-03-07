5705b7a343304bd416417f34434e10b3e9dfa033
olivergirard <girard1@purdue.edu>
2024-03-06 21:02:29 -0500

    worked on main

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw.h"
+#include "ImGui/imgui_impl_opengl3.h"
+
+int main() {
+
+	return 0;
+}
